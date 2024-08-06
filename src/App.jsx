b7be312cda2a0477d045270c47c3c3edef89325c
lokhandeganesh <lokhandeganesh8@gmail.com>
2024-08-06 10:54:39 +0530

    setState with setTimeout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import React from 'react';
+import Final from './tutorial/01-useState/final/03-useState-array'
+import ErrorExample from './components/ErrorExample';
+import UseStateArray from './components/UseStateArray';
+import UseStateObject from './components/UseStateObject';
+
 function App() {
   return (
-    <div className='container'>
-      <h2>Advanced React</h2>
-    </div>
+    <React.Fragment>
+      <div className='container'>
+        {/* <ErrorExample></ErrorExample> */}
+        {/* <UseStateArray></UseStateArray> */}
+        <UseStateObject></UseStateObject>
+        {/* <Final></Final> */}
+      </div>
+    </React.Fragment>
   );
 }
 
