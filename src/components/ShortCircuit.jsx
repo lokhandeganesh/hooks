d3c0445a586f712a6ca4baacd3cb21c36f5b7f7e
lokhandeganesh <lokhandeganesh8@gmail.com>
2024-08-07 14:42:19 +0530

    conditional component

--- src/components/ShortCircuit.jsx
+++ src/components/ShortCircuit.jsx
@@ -1,20 +1,38 @@
 import React, { useState } from "react";
 
 const ShortCircuit = () => {
-  const [text, setText] = useState('')
-  const [name, setName] = useState('Ganesh')
-
+  const [text, setText] = useState('');
+  const [name, setName] = useState('Ganesh');
+  const [user, setUser] = useState({ name: 'john' });
+  const [isEditing, setIsEditing] = useState(false);
 
   return (
     <div>
       {/* {if(someCondition) {"won't work"}} */}
       <h2>Short Circuit</h2>
-      <h4>Falsy OR: {text || 'Hello World'}</h4>
-      <h4>Falsy AND: {text && 'Hello World'}</h4>
-      <h4>Truthy OR: {name || 'Hello World'}</h4>
-      <h4>Truthy AND: {name && 'Hello World'}</h4>
+      <h4>{text || 'default value'}</h4>
+
+      {text && (
+        <h2>{name}</h2>
+      )}
+
+      {/*
+      {!text && (
+        <h2>{name}</h2>
+      )} */}
+
+      {user && <NameComponent name={user.name}></NameComponent>}
     </div>
   )
-}
+};
+
+const NameComponent = ({ name }) => {
+  return (
+    <div>
+      <h3>Name Component</h3>
+      <h4>{name || 'default value'}</h4>
+    </div>
+  )
+};
 
 export default ShortCircuit
