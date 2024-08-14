6581390314aafa5a966adf7b16a2373e53ab1303
lokhandeganesh <lokhandeganesh8@gmail.com>
2024-08-14 12:45:13 +0530

    useReducer setup

--- hooks/src/components/useReducer/useReducerBasic.jsx
+++ hooks/src/components/useReducer/useReducerBasic.jsx
@@ -1,22 +1,45 @@
-import React, { useState } from 'react'
-import { data } from '../../data'
+import React, { useState, useReducer } from 'react'
+import { data, people } from '../../data'
+
+const defaultState = {
+  people: data,
+  isLoading: false
+}
+
+const reducer = (state, action) => {
+  // console.log(action)
+  if (action.type === 'CLEAR_ITEM') {
+    return { ...state, people: [] };
+  }
+
+}
+
 
 const ReducerBasic = () => {
-  const [people, setPeople] = useState(data);
+  const [state, dispatch] = useReducer(reducer, defaultState);
+
+  // const [people, setPeople] = useState(data);
 
   const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id != id);
-    setPeople(newPeople);
+    // let newPeople = people.filter((person) => person.id != id);
+    // setPeople(newPeople);
+  };
+
+  const clearItem = () => {
+    dispatch({ type: 'CLEAR_ITEM' })
+    // setPeople([])
   };
 
   const resetItem = () => {
-    setPeople(data);
-  }
+    // setPeople(data)
+  };
+
+  console.log(state)
 
   return (
     <div>
       {
-        people.map((person) => {
+        state.people.map((person) => {
           const { id, name } = person;
           return (
             <div key={id} className='item'>
@@ -28,7 +51,7 @@ const ReducerBasic = () => {
       }
 
       {
-        people.length < 1 ?
+        state.people.length < 1 ?
           (
             <button className='btn'
               style={{ marginTop: '2rem' }}
@@ -39,7 +62,7 @@ const ReducerBasic = () => {
             <button
               className='btn'
               style={{ marginTop: '2rem' }}
-              onClick={() => { setPeople([]) }}>Clear</button>
+              onClick={clearItem}>Clear</button>
           )
       }
     </div >
