d9867bcc06e6305c6ea6961a3e829b501babb8e0
lokhandeganesh <lokhandeganesh8@gmail.com>
2024-11-06 18:22:50 +0530

    sanity.io setup initiated

---
+++ figma-to-react/server/studio-figma-to-react/schemaTypes/testimonials.js
@@ -0,0 +1,26 @@
+export default {
+  name: "testimonials",
+  title: "Testimonials",
+  type: "document",
+  fields: [{
+    name: "image",
+    type: "image",
+    title: "Image"
+  },
+  {
+    name: "name",
+    type: "string",
+    title: "Name"
+  },
+  {
+    name: "company",
+    type: "string",
+    title: "company"
+  },
+  {
+    name: "message",
+    type: "text",
+    title: "Message"
+  },
+  ]
+}
