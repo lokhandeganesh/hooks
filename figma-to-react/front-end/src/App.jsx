7a768e7188494d9972ed8578c85f3187550d8de5
lokhandeganesh <lokhandeganesh8@gmail.com>
2024-11-06 12:54:40 +0530

    bugs in footer.jsx file

--- figma-to-react/front-end/src/App.jsx
+++ figma-to-react/front-end/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Header, Topics, Info, Blog, Testimonials, Ftr } from './components'
+import { Header, Topics, Info, Blog, Testimonials, Footer } from './components'
 
 const App = () => {
   return (
@@ -9,7 +9,7 @@ const App = () => {
       <Info></Info>
       <Blog></Blog>
       <Testimonials></Testimonials>
-      <Ftr></Ftr>
+      <Footer></Footer>
     </>
 
   )
--- figma-to-react/front-end/src/components/index.js
+++ figma-to-react/front-end/src/components/index.js
@@ -4,6 +4,5 @@ import Info from "./Info/Info";
 import Blog from "./Blog/Blog";
 import Testimonials from "./Testimonials/Testimonials"
 import Footer from "./Footer/Footer"
-// import Ftr from "./f/F"
 
-export { Header, Topics, Info, Blog, Testimonials, Footer }//, Ftr }
+export { Header, Topics, Info, Blog, Testimonials, Footer }
