da9b90eb33ae927f30972828e883f5ffaa8a9334
lokhandeganesh <lokhandeganesh8@gmail.com>
2024-11-06 17:30:25 +0530

    applied click event on mobile version

--- figma-to-react/front-end/src/components/Navbar/Navbar.jsx
+++ figma-to-react/front-end/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Navbar.css"
-
+import { menuIcon, closeIcon } from "../../assets"
 const Navbar = () => {
+  const [isActive, setIsActive] = useState(false);
   return (
-    <nav>
-      <a href="#" className='logo'>Sound<span className='red'>DZign</span></a>
-      <ul>
-        <li><a href="#">Course Details</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Blog</a></li>
-        <li><a href="#">Testimonials</a></li>
-      </ul>
-    </nav>
+    <>
+      <nav>
+        <a href="#" className='logo'>Sound<span className='red'>DZign</span></a>
+        <ul>
+          <li><a href="#topics">Course Details</a></li>
+          <li><a href="#info">About</a></li>
+          <li><a href="#blog">Blog</a></li>
+          <li><a href="#testimonials">Testimonials</a></li>
+        </ul>
+        <div className="menu-icon" onClick={() => setIsActive(true)}>
+          <img src={menuIcon} alt="" />
+        </div>
+      </nav>
+      <div className={`mobile-menu-container ${isActive ? "active" : ""}`}>
+        <div className="close-icon" onClick={() => setIsActive(false)}>
+          <img src={closeIcon} alt="" />
+        </div>
+        <ul className='menu-items'>
+          <li><a href="#topics" onClick={() => setIsActive(false)}>Course Details</a></li>
+          <li><a href="#info" onClick={() => setIsActive(false)}>About</a></li>
+          <li><a href="#blog" onClick={() => setIsActive(false)}>Blog</a></li>
+          <li><a href="#testimonials" onClick={() => setIsActive(false)}>Testimonials</a></li>
+        </ul>
+      </div>
+    </>
   )
 }
 
