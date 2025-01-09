45c4660e841c682db5fd6e5da355e6193bc90eed
lokhandeganesh <lokhandeganesh8@gmail.com>
2025-01-09 18:41:43 +0530

    implemented social media icons from data.js

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
-import { links, social } from './data'
+import { FaBars } from 'react-icons/fa'
+import { links, socials } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
@@ -14,42 +14,33 @@ const Navbar = () => {
           </button>
           <div className="links-container show-container">
             <ul className="links">
-              <li>
-                <a href="#">
-                  home
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  about
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  contact
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  product
-                </a>
-              </li>
+              {
+                links.map((link) => {
+                  const { id, url, text } = link;
+                  return (
+                    <li key={id}>
+                      <a href={url}>{text}</a>
+                    </li>
+                  );
+                })
+              }
             </ul>
           </div>
         </div>
         <ul className="social-icons">
-          <li>
-            <a href="https://twitter.com"><FaTwitter /></a>
-          </li>
-          <li>
-            <a href="https://twitter.com"><FaTwitter /></a>
-          </li>
-          <li>
-            <a href="https://twitter.com"><FaTwitter /></a>
-          </li>
+          {
+            socials.map((social) => {
+              const { id, url, icon } = social;
+              return (
+                <li key={id}>
+                  <a href={url}>{icon}</a>
+                </li>
+              );
+            })
+          }
         </ul>
       </div>
-    </nav>
+    </nav >
   )
 }
 
