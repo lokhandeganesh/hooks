ef549f4f4cddbcfea2a608c9a226aca4abc16ec8
lokhandeganesh <lokhandeganesh8@gmail.com>
2025-01-13 13:07:47 +0530

    setup custom hook for importing data to all modules

--- 12-sidebar-modal/setup/src/Home.js
+++ 12-sidebar-modal/setup/src/Home.js
@@ -1,7 +1,15 @@
 import React, { useContext } from 'react'
 import { FaBars } from 'react-icons/fa'
+import { AppContext, useGlobalContext } from './context'
+
 
 const Home = () => {
+  // const data = useContext(AppContext);
+
+  // imported from custom hook
+  const data = useGlobalContext();
+  console.log(data);
+
   return (
     <main>
       <button className="sidebar-toggle">
